f49e1b7b01d205dbbfcddec0649c36bcf870f8c6
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-01 20:54:26 +0000

    pycodestyle fixed

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,5 +4,9 @@ This module defines a type-annotated function that takes two strings as
 arguments and returns a concatenated string
 """
 
-def concat(str1: str, str2: str) ->str:
+
+def concat(str1: str, str2: str) -> str:
+    """
+    type-annotated function returns a concatenated string
+    """
     return str1 + str2
