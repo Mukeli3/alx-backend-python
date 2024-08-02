f9b61668476c47d603bc9bd2f1ef3bbf37c7f709
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-02 00:40:42 +0000

    few adjustments

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 This module defines a function which takes a list of floats as argument
 and retuns their sum as float
@@ -5,4 +6,10 @@ and retuns their sum as float
 
 
 def sum_list(input_list: list) -> float:
+    """
+    Args:
+       input_list - list
+    Return:
+       sum as a float
+    """
     return sum(input_list)
