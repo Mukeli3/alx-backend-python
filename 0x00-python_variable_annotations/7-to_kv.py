e104c175bee1c1389267c3c1ed5517f02646ac59
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-02 01:10:19 +0000

    pycodestyle and first line fixed

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from typing import Tuple, Union
 
 """
@@ -9,4 +10,13 @@ and should be annotated as a float
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Converts a key-value pair to a tuple.
+
+  Args:
+    k: The key as a string.
+    v: The value as an integer or float.
+
+  Returns:
+    A tuple containing the key and the square of the value as a float.
+  """
     return k, v ** 2
