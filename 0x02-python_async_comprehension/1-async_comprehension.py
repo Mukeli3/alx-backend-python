f091bf5d4ee09fe128f697987d88a38a9ad6010c
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-08 23:57:50 +0000

    removed whitespace

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,8 +6,7 @@ and returns the random numbers
 """
 import asyncio
 from typing import List
-async_generator = __import__ ('0-async_generator').async_generator
-
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
